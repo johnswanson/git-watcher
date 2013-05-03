8369671783eb21c11dd8467c77fb8e3dcb368f88
John Swanson <john.d.swanson@gmail.com>
2013-05-03 16:54:58 -0700

    initial commit

---
+++ watcher.js
@@ -0,0 +1,37 @@
+#! /usr/local/bin/node
+
+var fs = require("fs");
+var child = require("child_process");
+var path = require("path");
+
+var watchPath = process.argv[2] || process.cwd();
+watchPath = path.join(watchPath, ".git");
+
+fs.watch(watchPath, showLog);
+process.stdout.on("resize", showLog);
+
+function showLog() {
+  var cols = process.stdout.columns,
+      rows = process.stdout.rows,
+      command = [
+        "git",
+        "--no-pager",
+        "log",
+        "--no-color",
+        "--graph",
+        "--pretty=format:'%h %d %s (%cr) <%an>'",
+        // "--pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'",
+        "--abbrev-commit",
+        "--branches"
+      ].join(" ");
+
+  child.exec(command, {cwd: watchPath}, function(err, stdout, stderr) {
+    if (err !== null) return;
+
+    process.stdout.write("\u001B[2J\u001B[0;0f");
+    stdout = stdout.split("\n").slice(0, rows - 2).map(function (l) {
+      return l.substring(0, cols);
+    }).join("\n");
+    process.stdout.write(stdout);
+  });
+}
