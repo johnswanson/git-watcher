6c8e24e19a12e80c8dc5bf955e244c6018d8e382
John Swanson <john.d.swanson@gmail.com>
2013-05-03 17:46:56 -0700

    cache log, nonblock exec of git-log

--- watcher.js
+++ watcher.js
@@ -4,16 +4,38 @@ var fs = require("fs");
 var child = require("child_process");
 var path = require("path");
 
+var LOG_CACHE = "";
+
 var watchPath = process.argv[2] || process.cwd();
 watchPath = path.join(watchPath, ".git");
 
-fs.watch(watchPath, showLog);
 process.stdout.on("resize", showLog);
 
-function showLog() {
+fs.watch(watchPath, function () {
+  getLog(showLog);
+});
+
+process.stdout.on("resize", function () {
+  if (LOG_CACHE !== "") showLog(null, LOG_CACHE);
+  else getLog(showLog);
+});
+
+function showLog(err, log) {
+  if (err !== null) return;
   var cols = process.stdout.columns,
-      rows = process.stdout.rows,
-      command = [
+      rows = process.stdout.rows;
+  process.stdout.write(formatTTY(log, cols, rows));
+}
+
+function formatTTY(string, cols, rows) {
+  var out = "\u001B[2J\u001B[0;0f";
+  return out + string.split("\n").slice(0, rows).map(function (l) {
+    return l.substring(0, cols);
+  }).join("\n");
+}
+
+function getLog(callback) {
+  var command = [
         "git",
         "--no-pager",
         "log",
@@ -25,12 +47,7 @@ function showLog() {
       ].join(" ");
 
   child.exec(command, {cwd: watchPath}, function(err, stdout, stderr) {
-    if (err !== null) return;
-
-    process.stdout.write("\u001B[2J\u001B[0;0f");
-    stdout = stdout.split("\n").slice(0, rows - 2).map(function (l) {
-      return l.substring(0, cols);
-    }).join("\n");
-    process.stdout.write(stdout);
+    if (err !== null) callback(err);
+    else callback(null, stdout);
   });
 }
